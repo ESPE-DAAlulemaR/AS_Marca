27120c73ee42747c4136b50a1d4120386c34612d
Dannyel <dannyel.alulema.2001@gmail.com>
2025-01-25 20:17:15 -0500

    FIX: Arreglar dependencia (feing)

--- src/main/java/com/banquito/marca/aplicacion/controlador/dto/peticion/TarjetaPeticionDTO.java
+++ src/main/java/com/banquito/marca/aplicacion/controlador/dto/peticion/TarjetaPeticionDTO.java
@@ -1,11 +1,17 @@
 package com.banquito.marca.aplicacion.controlador.dto.peticion;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class TarjetaPeticionDTO {
+    @NotNull
+    @NotEmpty
+    @Size(min = 10, max = 13, message = "El número de identificación tiene maximo 10 caracteres y minimo 10")
     private String identificacion;
     private String nombre;
     private String direccion;
