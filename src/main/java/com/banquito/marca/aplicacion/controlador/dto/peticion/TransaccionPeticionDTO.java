ac5119e9b2d3861adc49f591f2ec44a680d104aa
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2025-01-26 19:22:44 -0500

    RF - Validaciones TransaccionPeticionDTO

--- src/main/java/com/banquito/marca/aplicacion/controlador/dto/peticion/TransaccionPeticionDTO.java
+++ src/main/java/com/banquito/marca/aplicacion/controlador/dto/peticion/TransaccionPeticionDTO.java
@@ -1,6 +1,7 @@
 package com.banquito.marca.aplicacion.controlador.dto.peticion;
 
 import com.banquito.marca.aplicacion.controlador.dto.DetalleJsonDTO;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,14 +10,30 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 public class TransaccionPeticionDTO {
+    @NotBlank
+    @Size(min =0, max=16, message = "Numero de la tarjeta difiere del tamaño permitido")
     private String numeroTarjeta;
+    @NotBlank
+    @Size(min = 0, max = 3, message = "El CVV tiene que ser de un maximo de 3 caracteres")
     private String cvv;
+    @NotEmpty
     private String fechaCaducidad;
+    @NotEmpty
     private BigDecimal valor;
+    @NotBlank
+    @Size(min = 0, max = 100)
     private String descripcion;
+    @NotBlank
+    @Size(min = 0, max = 100)
     private String beneficiario;
+    @NotBlank
     private String numeroCuenta;
+    @NotEmpty
     private Boolean esDiferido;
+    @NotEmpty
+    @Positive
     private Integer cuotas;
+    @NotEmpty
+    @Size(min = 0, max = 100)
     private DetalleJsonDTO detalle;
 }
