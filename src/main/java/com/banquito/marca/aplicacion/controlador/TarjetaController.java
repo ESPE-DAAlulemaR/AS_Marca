219d4327c64846b4b47b085061217451eb53866f
ldgomez3 <ldgomez3@espe.edu.ec>
2025-01-27 01:14:20 -0500

    DG - Se actualizo dependencias y configuracion para el Cors

--- src/main/java/com/banquito/marca/aplicacion/controlador/TarjetaController.java
+++ src/main/java/com/banquito/marca/aplicacion/controlador/TarjetaController.java
@@ -14,17 +14,15 @@ import com.banquito.marca.compartido.utilidades.UtilidadHash;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("v1/tarjetas")
+@RequestMapping("/v1/tarjetas")
+@CrossOrigin("*")
 public class TarjetaController {
     private final TarjetaService tarjetaService;
     private final ITarjetaPeticionMapper tarjetaPeticionMapper;
--- src/main/java/com/banquito/marca/aplicacion/controlador/TransaccionController.java
+++ src/main/java/com/banquito/marca/aplicacion/controlador/TransaccionController.java
@@ -1,7 +1,6 @@
 package com.banquito.marca.aplicacion.controlador;
 
 import com.banquito.marca.aplicacion.controlador.dto.peticion.TransaccionPeticionDTO;
-import com.banquito.marca.aplicacion.controlador.dto.respuesta.TarjetaRespuestaDTO;
 import com.banquito.marca.aplicacion.controlador.dto.respuesta.TransaccionRespuestaDTO;
 import com.banquito.marca.aplicacion.controlador.mapper.ITransaccionPeticionMapper;
 import com.banquito.marca.aplicacion.controlador.mapper.ITransaccionRespuestaMapper;
@@ -15,32 +14,24 @@ import com.banquito.marca.compartido.excepciones.OperacionInvalidaExcepcion;
 import jakarta.validation.Valid;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 
-//import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Sort;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/transacciones")
+@CrossOrigin("*")
 public class TransaccionController {
     private final TransaccionService transaccionService;
     private final ITransaccionPeticionMapper transaccionPeticionMapper;
@@ -51,7 +42,6 @@ public class TransaccionController {
 
     private final ValidadorTarjetasService validadorTarjetasService;
 
-    private final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
 
     public TransaccionController(
             TransaccionService transaccionService,
@@ -87,33 +77,24 @@ public class TransaccionController {
     }
 
     @GetMapping
-    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Page<TransaccionRespuestaDTO>> listar(
             @RequestHeader(value = "X-Idempotency-Key", required = false) String idempotencyKey, 
             @PageableDefault(size = 20, sort = "fechaHora", direction = Sort.Direction.DESC) Pageable pageable,
             @RequestParam(required = false) String estado,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaDesde,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fechaHasta,
-            @RequestParam(required = false) String numeroTarjeta) {
-        // Si el header no está presente, genera un ID aleatorio
-        if (idempotencyKey == null || idempotencyKey.isEmpty()) {
+            @RequestParam(required = false) String numeroTarjeta
+    ) {
+        if (idempotencyKey == null || idempotencyKey.isEmpty())
             idempotencyKey = UUID.randomUUID().toString();
-        }
 
-        // Verificar si la respuesta ya está en caché
         Page<TransaccionRespuestaDTO> cachedResponse = responseCache.getIfPresent(idempotencyKey);
-        if (cachedResponse != null) {
+        if (cachedResponse != null)
             return ResponseEntity.ok(cachedResponse);
-        }
 
-        // Obtener transacciones
         Page<Transaccion> transacciones = transaccionService.listarTransacciones(
                 estado, fechaDesde, fechaHasta, numeroTarjeta, pageable);
-
-        // Convertir a DTO
         Page<TransaccionRespuestaDTO> respuesta = transacciones.map(transaccionRespuestaMapper::toDto);
-
-        // Guardar en caché con un idempotencyKey válido
         responseCache.put(idempotencyKey, respuesta);
 
         return ResponseEntity.ok(respuesta);
