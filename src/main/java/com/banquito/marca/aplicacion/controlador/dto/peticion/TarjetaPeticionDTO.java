ab2159a09bfc44bc4950e5813d49abedc1f7f6af
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2025-01-26 19:00:30 -0500

    RF - Validaciones TarjetaPeticionDTO

--- src/main/java/com/banquito/marca/aplicacion/controlador/dto/peticion/TarjetaPeticionDTO.java
+++ src/main/java/com/banquito/marca/aplicacion/controlador/dto/peticion/TarjetaPeticionDTO.java
@@ -1,22 +1,31 @@
 package com.banquito.marca.aplicacion.controlador.dto.peticion;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class TarjetaPeticionDTO {
-    @NotNull
-    @NotEmpty
+    @NotBlank
     @Size(min = 10, max = 13, message = "El número de identificación tiene maximo 10 caracteres y minimo 10")
     private String identificacion;
+    @NotBlank
+    @Size(min = 3, max = 100)
     private String nombre;
+    @NotBlank
+    @Size(min = 0, max = 50)
     private String direccion;
+    @NotBlank
+    @Size(min = 3, max = 50)
     private String telefono;
+    @NotEmpty
+    @Email
     private String correo;
+    @NotBlank
+    @Size(min = 0, max = 3)
     private String tipo;
+    @NotBlank
+    @Size(min = 3, max = 100)
     private String franquicia;
 }
