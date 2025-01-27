212e7758dd85516980867e92ca20d3a2dee1a237
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2025-01-27 09:09:59 -0500

    Update TarjetaPeticionDTO.java

--- src/main/java/com/banquito/marca/aplicacion/controlador/dto/peticion/TarjetaPeticionDTO.java
+++ src/main/java/com/banquito/marca/aplicacion/controlador/dto/peticion/TarjetaPeticionDTO.java
@@ -8,24 +8,30 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TarjetaPeticionDTO {
     @NotBlank(message = "El número de identificación no debe ser vacío ni nulo")
-    @Size(min = 10, max = 13, message = "El número de identificación debe tener entre 10 y 13 caracteres")
+    @Size(max = 13, message = "El número de identificación debe tener entre 10 y 13 caracteres")
     private String identificacion;
+
     @NotBlank(message = "El nombre no debe ser vacío ni nulo")
-    @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres")
+    @Size(max = 100, message = "El nombre debe tener entre 3 y 100 caracteres")
     private String nombre;
+
     @NotBlank(message = "La dirección no debe ser vacía ni nula")
-    @Size(min = 5, max = 50, message = "La dirección debe tener un máximo de 50 caracteres")
+    @Size(max = 50, message = "La dirección debe tener un máximo de 50 caracteres")
     private String direccion;
+
     @NotBlank(message = "El teléfono no debe ser vacío ni nulo")
     @Size(max = 13, message = "El teléfono debe tener 13 caracteres")
     private String telefono;
+
     @NotBlank(message = "El correo no debe ser vacío ni nulo")
     @Email(message = "El correo debe tener un formato válido (ejemplo@dominio.com)")
     private String correo;
+
     @NotBlank(message = "El tipo de tarjeta no debe ser vacío ni nulo")
     @Size(max = 3, message = "El tipo debe tener un máximo de 3 caracteres")
     private String tipo;
+
     @NotBlank(message = "La franquicia no debe ser vacía ni nula")
-    @Size(min = 4, max = 10, message = "La franquicia debe tener entre 4 y 10 caracteres")
+    @Size(max = 10, message = "La franquicia debe tener entre 4 y 10 caracteres")
     private String franquicia;
 }
