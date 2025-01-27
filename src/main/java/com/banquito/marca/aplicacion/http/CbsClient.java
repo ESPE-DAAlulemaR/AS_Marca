0971d1d211be0c65652bb49124023499e8c7b172
Dannyel <dannyel.alulema.2001@gmail.com>
2025-01-27 00:15:54 -0500

    Primer consumo

--- src/main/java/com/banquito/marca/aplicacion/http/CbsClient.java
+++ src/main/java/com/banquito/marca/aplicacion/http/CbsClient.java
@@ -1,11 +1,15 @@
 package com.banquito.marca.aplicacion.http;
 
+import com.banquito.marca.aplicacion.controlador.dto.peticion.TransaccionPeticionDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+import java.util.Map;
+
 @FeignClient(name = "CBS-API", url = "${external.cbs.api.base-url}")
 public interface  CbsClient {
-    //@RequestMapping(method = RequestMethod.POST, value = "/v1/transacciones")
-    //List<Post> getPosts();
+    @RequestMapping(method = RequestMethod.POST, value = "/v1/transacciones")
+    List<Map<String, Object>> enviarTransaccionBanco(TransaccionPeticionDTO transaccion);
 }
