92b54d9d551beec1cc5d4b2630e904a91e2e69db
Rafael Fonseca <58662403+rafa-fz@users.noreply.github.com>
2025-01-26 21:51:40 -0500

    Update ItemComisionDTO.java

--- src/main/java/com/banquito/marca/aplicacion/controlador/dto/ItemComisionDTO.java
+++ src/main/java/com/banquito/marca/aplicacion/controlador/dto/ItemComisionDTO.java
@@ -1,9 +1,6 @@
 package com.banquito.marca.aplicacion.controlador.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,12 +9,13 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 public class ItemComisionDTO {
-    @NotBlank
+    @NotBlank(message = "La referencia no debe ser vacía ni nula")
+    @Size(min = 1, max = 100, message = "La referencia debe tener un máximo de 100 caracteres")
     private String referencia;
-    @NotEmpty
-    @Positive
+    @NotNull(message = "La comisión no debe ser nula")
+    @PositiveOrZero(message = "La comisión debe ser un valor positivo o cero")
     private BigDecimal comision;
-    @NotEmpty
-    @Size(min = 0, max = 8)
+    @NotBlank(message = "El número de cuenta no debe ser vacío ni nulo")
+    @Size(min = 1, max = 50, message = "El número de cuenta debe tener un máximo de 50 caracteres")
     private String numeroCuenta;
 }
